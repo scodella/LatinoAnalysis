3ce3df4ef93e08cfbef1139883c28e748bb1f678
Pablo <pablo.matorras@cern.ch>
2021-03-23 11:47:29 +0100

    remove lowers() and readd unclustered energy

--- NanoGardener/python/modules/mt2Producer.py
+++ NanoGardener/python/modules/mt2Producer.py
@@ -177,13 +177,15 @@ class mt2Producer(Module):
             if nLooseLeptons==2 and leptons[lepLoose[0]].pdgId*leptons[lepLoose[1]].pdgId<0: return False
  
         ptmissvec3 = ROOT.TVector3()
-        if 'jer' in self.metSystematic.lower() or 'smear' in self.metSystematic.lower():
+        if 'unclust' in self.metSystematic:
+            metBranch='MET'
+        if 'jer' in self.metSystematic or 'Smear' in self.metSystematic:
             metBranch='MET_T1Smear'
         else:
             metBranch = 'MET_T1' 
         if hasattr(event, 'METFixEE2017_pt_nom'): metBranch = 'METFixEE2017' 
         if self.metType=='puppi':  metBranch = 'PuppiMET' 
-        metSystem = '_'+self.metSystematic#.replace('Smear' , '') 
+        metSystem = '_'+self.metSystematic.replace('Smear' , '') 
         if metSystem == '_jer' or metSystem=='_nom' : metSystem = ''
         
         if not hasattr(event, metBranch+'_pt'+metSystem):
@@ -194,7 +196,7 @@ class mt2Producer(Module):
 
         ptmissvec3.SetPtEtaPhi(getattr(event, metBranch+'_pt'+metSystem), 0., getattr(event, metBranch+'_phi'+metSystem)) 
 
-        if 'Smear' in self.metSystematic and "unclust" in self.metSystematic.lower()  :
+        if "unclustEnSmear" in self.metSystematic :
             ptmissnom = ROOT.TVector3()
             ptmissjer = ROOT.TVector3()
 
