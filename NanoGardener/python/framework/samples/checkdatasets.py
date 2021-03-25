89d4bc079daeb134d8c1d52e9c7264de33454308
Pablo <pablo.matorras@cern.ch>
2021-03-25 12:37:15 +0100

    update the dataset check to be updatable, and improve some capabilities

--- NanoGardener/python/framework/samples/checkdatasets.py
+++ NanoGardener/python/framework/samples/checkdatasets.py
@@ -37,10 +37,24 @@ campaigns = { 'UL16APV' : { 'MC'   : { 'AODSIM' : 'RunIISummer20UL16RECOAPV', 'M
                           'FS'   : { 'AODSIM' : '',                       'MINIAODSIM' : '',                          'NANOAODSIM' : ''                          , 'GEN' : ''                      }, }, 
             }
 
-# Main
+#Get the latest version if requested
+def renewSampleFile(filename):
+    os.system('wget --output-file="logs.txt" "https://docs.google.com/spreadsheets/d/1ABl2p2uwr2EfEbolBEVNcKb_fIXigYY9sqCRT8XIi1Q/export?format=csv&gid=1318927481" -O "downloaded_content.csv"')
+    with open("downloaded_content.csv", "r") as file_input:
+        with open(filename, "w") as output:
+            isHeader=True
+            for i,line in enumerate(file_input):
+                if 'Total' and 'Samples' in line.split(',')[0]: #More refined feature can be added, but works now
+                    isHeader=False
+                    continue
+                
+                if isHeader is False:
+                    output.write(line)
+
+#Read Csv file
 def readSampleFile(filename):
     samhere=[]
-    with open("Summer20ULPlanning.csv") as f:
+    with open(filename) as f:
         for row in f:
             samhere.append(row.split(",")[0])
     return samhere
@@ -54,23 +68,27 @@ def substringinlist(sample_list,substring):
             #print "satisfies", substring, item
     return list(inlist)
 
+# Main
 if __name__ == '__main__':
 
-    csvsamples = readSampleFile("Summer20ULPlanning.csv")
     # Input parameters
     usage = 'usage: %prog [options]'
     parser = optparse.OptionParser(usage)
 
-    parser.add_option('-d', '--directory' , dest='directory' , help='CMSSW directory', default=cmssw_directory)
-    parser.add_option('-s', '--samplefile', dest='samplefile', help='Sample file'    , default='Run2016_102X_nAODv6')
-    parser.add_option('-c', '--campaign'  , dest='campaign',   help='Campaign'       , default='UL16')
-    parser.add_option('-t', '--tier'      , dest='tier',       help='Tier'           , default='nanoAOD')
-    parser.add_option('-o', '--outputfile', dest='outputfile', help='Output file'    , default='test')
-    parser.add_option('-m', '--mute'      , dest='mute'      , help='mute'           , default=False, action='store_true')
-    parser.add_option('-l', '--list'      , dest='list'      , help='List in csv'    , default=False, action='store_true')
+    parser.add_option('-d', '--directory' , dest='directory' , help='CMSSW directory' , default=cmssw_directory)
+    parser.add_option('-s', '--samplefile', dest='samplefile', help='Sample file'     , default='Run2016_102X_nAODv6')
+    parser.add_option('-c', '--campaign'  , dest='campaign',   help='Campaign'        , default='UL16')
+    parser.add_option('-t', '--tier'      , dest='tier',       help='Tier'            , default='nanoAOD')
+    parser.add_option('-o', '--outputfile', dest='outputfile', help='Output file'     , default='test')
+    parser.add_option('-m', '--mute'      , dest='mute'      , help='mute'            , default=False, action='store_true')
+    parser.add_option('-l', '--list'      , dest='list'      , help='List in csv'     , default=False, action='store_true')
+    parser.add_option('-n', '--newcsv'    , dest='newcsv'    , help='Download new csv', default=False, action='store_true')
+    
     (opt, args) = parser.parse_args()
-
-    csvsamples = readSampleFile("Summer20ULPlanning.csv")
+    
+    csvfile = "Summer20ULPlanning.csv"
+    if opt.newcsv: renewSampleFile(csvfile)
+    csvsamples = readSampleFile(csvfile)
 
     if opt.samplefile==opt.outputfile:
         print 'Error: overwriting input file', opt.samplefile
@@ -194,17 +212,20 @@ if __name__ == '__main__':
                 version = 0
                 saveset = ''
                 for dataset in datasetsFound:  
-                    if len(dataset.split('-v'))>1: 
-
-                        if (version < int(dataset.split('-v')[1].split('/')[0])):
+                    #print "DATASET", dataset, dataset.split('-v')
+                    if len(dataset.split('ver'))>1: ver = 'ver'
+                    else:  ver = 'v'
+                    if len(dataset.split(ver))>1: 
+                        print dataset.split(ver)[1], ver,  dataset.split(ver)[1][0]
+                        if (version < int(dataset.split(ver)[1][0])):
                             #print "new sample", dataset, version
                             saveset=dataset
-                        elif (version == int(dataset.split('-v')[1].split('/')[0])):
+                        elif (version == int(dataset.split(ver)[1][0])):
                             print "WARNING: "+ dataset+" and "+saveset+" have the same version" 
                     else: print "TRY DIFFERENT CODING" #May have to be updated in the future
                 if verbose: print 'Dataset picked for sample', process+period, 'in campaign', campaign[thistier], '-->', saveset
-                
-                status = 'NanoAODv2 ready:, ' + saveset
+                datasetFound = saveset
+                status       = 'NanoAODv2 ready:, ' + saveset
             else:   
                 if verbose: 
                     print 'Warning: no dataset found for sample', process+period, 'in tier', opt.tier, 'for campaign', campaign[thistier] 
@@ -331,7 +352,7 @@ if __name__ == '__main__':
                     print "................................\n", datasetFound
                     exit()
             
-
+            print "STATUS", status
             if len(datasetFound) == 0 : 
                 datasetFlag = ''
                 line+='#'
@@ -355,6 +376,6 @@ if __name__ == '__main__':
             OutputSamples[sampleName] = { }
             OutputSamples[sampleName][opt.tier] = datasetFound
             line+="Samples[\'"+sampleName+"\'] \t = {\'"+opt.tier+"\': \'"+datasetFound+"\'}"
-            if "#" not in line: print line
+            if "#" not in line: print "Add to line", line
             writeList.write(line)
             
