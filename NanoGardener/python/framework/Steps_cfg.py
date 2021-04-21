455cf993475a979023c675e8262b86aa578051c2
Pablo Matorras <pablo.matorras@cern.ch>
2021-04-21 17:15:45 +0200

    remove prefire correction

--- NanoGardener/python/framework/Steps_cfg.py
+++ NanoGardener/python/framework/Steps_cfg.py
@@ -1452,7 +1452,7 @@ Steps = {
                      'isChain'    : True  ,
                      'do4MC'      : True  ,
                      'do4Data'    : False ,
-                     'subTargets' : ['baseW','PrefCorrUL18','btagPerJetDeepCSVWPs',
+                     'subTargets' : ['baseW','btagPerJetDeepCSVWPs',
                                      'rochesterMC','trigMC','LeptonSFSusy','puW','EmbeddingVeto',
                                      'wwNLOEWK','wzNLOEWK','zzNLOEWK','zNLOEWK','wNLOEWK','ZZGen' ],
                 },
