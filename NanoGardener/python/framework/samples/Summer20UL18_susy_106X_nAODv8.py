e70282dfc5a3f84fdaa24933d7bff5c827890fd3
Luca <luca.scodellaro@cern.ch>
2021-07-05 12:45:01 +0200

    More UL samples

--- NanoGardener/python/framework/samples/Summer20UL18_susy_106X_nAODv8.py
+++ NanoGardener/python/framework/samples/Summer20UL18_susy_106X_nAODv8.py
@@ -70,7 +70,7 @@ Samples['VBFHToTauTau_M125'] 	 = {'nanoAOD': '/VBFHToTauTau_M125_TuneCP5_13TeV-p
 #Samples['WWG'] 	 = {'nanoAOD': ''}
 Samples['WWTo2L2Nu'] 	 = {'nanoAOD': '/WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv2-106X_upgrade2018_realistic_v15_L1v1-v1/NANOAODSIM'}
 Samples['WWW'] 	 = {'nanoAOD': '/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv2-106X_upgrade2018_realistic_v15_L1v1-v1/NANOAODSIM'}
-#Samples['WWZ'] 	 = {'nanoAOD': ''}
+Samples['WWZ'] 	 = {'nanoAOD': '/WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv2-106X_upgrade2018_realistic_v15_L1v1-v1/NANOAODSIM'}
 
 #Samples['WZTo2L2Q'] 	 = {'nanoAOD': ''}
 #Samples['WZTo3LNu'] 	 = {'nanoAOD': ''}
