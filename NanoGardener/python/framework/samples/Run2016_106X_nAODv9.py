a356854814486d9fdc2c87b1c7d17012fba54be4
Pablo <pablo.matorras@cern.ch>
2021-08-24 15:53:30 +0200

    add missing samples (commented)

--- NanoGardener/python/framework/samples/Run2016_106X_nAODv9.py
+++ NanoGardener/python/framework/samples/Run2016_106X_nAODv9.py
@@ -3,8 +3,12 @@ Samples = {}
 Samples["DoubleEG_Run2016B-ver1_HIPM_UL2016-v1"]       = {'nanoAOD' : 'DoubleEG/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleEG_Run2016B-ver2_HIPM_UL2016-v1"]       = {'nanoAOD' : 'DoubleEG/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleEG_Run2016C_HIPM_UL2016-v1"]	       = {'nanoAOD' : 'DoubleEG/Run2016C-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["DoubleEG_Run2016D_HIPM_UL2016-v1"]            = {}
 Samples["DoubleEG_Run2016E_HIPM_UL2016-v1"]	       = {'nanoAOD' : 'DoubleEG/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleEG_Run2016F_UL2016-v1"] 	   	       = {'nanoAOD' : 'DoubleEG/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["DoubleEG_Run2016G_UL2016-v1"]                 = {}
+#Samples["DoubleEG_Run2016H_UL2016-v1"]                 = {}
+
 
 Samples["DoubleMuon_Run2016B-ver1_HIPM_UL2016-v1"]     = {'nanoAOD' : 'DoubleMuon/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleMuon_Run2016B-ver2_HIPM_UL2016-v1"]     = {'nanoAOD' : 'DoubleMuon/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
@@ -14,6 +18,7 @@ Samples["DoubleMuon_Run2016E_HIPM_UL2016-v1"] 	       = {'nanoAOD' : 'DoubleMuon
 Samples["DoubleMuon_Run2016F_HIPM_UL2016-v1"] 	       = {'nanoAOD' : 'DoubleMuon/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleMuon_Run2016F_UL2016-v1"] 	       = {'nanoAOD' : 'DoubleMuon/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleMuon_Run2016G_UL2016-v1"] 	       = {'nanoAOD' : 'DoubleMuon/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["DoubleMuon_Run2016H_UL2016-v1"]               = {}
 
 Samples["MET_Run2016B-ver1_HIPM_UL2016-v1"]	       = {'nanoAOD' : 'MET/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["MET_Run2016B-ver2_HIPM_UL2016-v1"] 	       = {'nanoAOD' : 'MET/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
@@ -21,8 +26,8 @@ Samples["MET_Run2016C_HIPM_UL2016-v1"] 		       = {'nanoAOD' : 'MET/Run2016C-HIP
 Samples["MET_Run2016D_HIPM_UL2016-v1"] 		       = {'nanoAOD' : 'MET/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["MET_Run2016E_HIPM_UL2016-v1"] 		       = {'nanoAOD' : 'MET/Run2016E-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["MET_Run2016F_HIPM_UL2016-v1"] 		       = {'nanoAOD' : 'MET/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-Samples["MET_Run2016F_UL2016-v1"] 	    	       = {'nanoAOD' : 'MET/Run2016F-UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD'}
-Samples["MET_Run2016F_UL2016_pilot-v1"]		       = {'nanoAOD' : 'MET/Run2016F-UL2016_MiniAODv2_NanoAODv9_pilot-v1/NANOAOD'}
+Samples["MET_Run2016F_UL2016-v2"] 	    	       = {'nanoAOD' : 'MET/Run2016F-UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD'}
+#Samples["MET_Run2016F_UL2016_pilot-v1"]		       = {'nanoAOD' : 'MET/Run2016F-UL2016_MiniAODv2_NanoAODv9_pilot-v1/NANOAOD'}
 Samples["MET_Run2016G_UL2016-v1"] 	    	       = {'nanoAOD' : 'MET/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["MET_Run2016H_UL2016-v1"] 	    	       = {'nanoAOD' : 'MET/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 
@@ -38,7 +43,9 @@ Samples["MuonEG_Run2016H_UL2016-v1"] 		       = {'nanoAOD' : 'MuonEG/Run2016H-UL
 
 Samples["SingleElectron_Run2016B-ver1_HIPM_UL2016-v1"] = {'nanoAOD' : 'SingleElectron/Run2016B-ver1_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["SingleElectron_Run2016B-ver2_HIPM_UL2016-v1"] = {'nanoAOD' : 'SingleElectron/Run2016B-ver2_HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["SingleElectron_Run2016C_HIPM_UL2016-v1"]      = {}
 Samples["SingleElectron_Run2016D_HIPM_UL2016-v1"]      = {'nanoAOD' : 'SingleElectron/Run2016D-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["SingleElectron_Run2016E_HIPM_UL2016-v1"]      = {}
 Samples["SingleElectron_Run2016F_HIPM_UL2016-v1"]      = {'nanoAOD' : 'SingleElectron/Run2016F-HIPM_UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["SingleElectron_Run2016F_UL2016-v1"] 	       = {'nanoAOD' : 'SingleElectron/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["SingleElectron_Run2016G_UL2016-v1"] 	       = {'nanoAOD' : 'SingleElectron/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
