73f9db77c211dbc5c1d94c60bae254e9002952bd
Pablo <pablo.matorras@cern.ch>
2021-08-24 18:38:05 +0200

    change cmssw tag to v8

--- NanoGardener/python/framework/Productions_cfg.py
+++ NanoGardener/python/framework/Productions_cfg.py
@@ -593,7 +593,7 @@ Productions = {
                        'isData'  : True ,
                        'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"  % os.environ["CMSSW_BASE"]',
                        'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2016_UL2016_HIPM_nAODv9.py' ,
-                       'cmssw'   : 'Full2016v9HIPM',
+                       'cmssw'   : 'Full2016v8HIPM',
                        'year'    : 'UL2016_preVFP' ,
                    },
 
@@ -601,7 +601,7 @@ Productions = {
                        'isData'  : True ,
                        'jsonFile'   : '"%s/src/LatinoAnalysis/NanoGardener/python/data/certification/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"  % os.environ["CMSSW_BASE"]',
                        'samples' : 'LatinoAnalysis/NanoGardener/python/framework/samples/Run2016_UL2016_noHIPM_nAODv9.py' ,
-                       'cmssw'   : 'Full2016v9noHIPM',
+                       'cmssw'   : 'Full2016v8noHIPM',
                        'year'    : 'UL2016' ,
                    },
 
