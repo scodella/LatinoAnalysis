9ddcffe946b583c84c5d22e3db9a2ed3769f1832
Luca <luca.scodellaro@cern.ch>
2021-09-01 13:56:13 +0200

    submitSusySteps: option for -T or -E

--- NanoGardener/scripts/submitSusySteps.sh
+++ NanoGardener/scripts/submitSusySteps.sh
@@ -9,8 +9,18 @@ if [ $step == 'sel' ]; then
 elif [ $step == 'data' ]; then
     queue=cms_main
 fi
+
+sample=''
+
 if [ $# -gt 2 ]; then
-    queue=cms_$3
+    if [[ $3 == *' '* ]]; then
+        sample=$3
+    else
+        queue=cms_$3
+    fi 
+    if [ $# -gt 3 ]; then
+        sample=$4
+    fi
 fi
 
 naod='nAODv8'
@@ -27,30 +37,30 @@ fi
 
 if [ $step == 'data' ]; then
 
-    ./mkPostProc.py -p Run20${year}_106X_${naod}_Full20${year}v8 -s DATASusy20${year}v8 -b -Q $queue -T MuonEG_Run2016C_HIPM_UL2016-v1
+    ./mkPostProc.py -p Run20${year}_106X_${naod}_Full20${year}v8 -s DATASusy20${year}v8 -b -Q $queue $sample
 
 elif [ $step == 'sel' ]; then
-    echo ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -s MCSusy20${year}v8 -b -Q $queue
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -s MCSusy20${year}v8 -b -Q $queue
+
+    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -s MCSusy20${year}v8 -b -Q $queue $sample
 
 elif [ $step == 'corr' ]; then
 
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8 -s MCSusyCorr20${year}v8 -b -Q $queue
+    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8 -s MCSusyCorr20${year}v8 -b -Q $queue $sample
 
 elif [ $step == 'syst' ]; then
 
     for syst in Nomin JESUp JESDo ; do
-        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8 -s MCSusy${syst}20${year}v8 -b -Q $queue
+        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8 -s MCSusy${syst}20${year}v8 -b -Q $queue $sample
     done
 
 elif [[ $step == 'reco' ]] || [[ $step == 'ctrl' ]] ; then
 
     for met in Nomin Smear SMTUp SMTDo ; do
-        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyNomin20${year}v8 -s susyMT2${step}$met -b -Q $queue
+        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyNomin20${year}v8 -s susyMT2${step}$met -b -Q $queue $sample
     done
 
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESUp20${year}v8 -s susyMT2${step}SJSUp -b -Q $queue
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESDo20${year}v8 -s susyMT2${step}SJSDo -b -Q $queue
+    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESUp20${year}v8 -s susyMT2${step}SJSUp -b -Q $queue $sample
+    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESDo20${year}v8 -s susyMT2${step}SJSDo -b -Q $queue $sample
 
 fi
 
