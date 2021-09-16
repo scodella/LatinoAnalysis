efbd0215a8336e2555936ee5330d3084a8e932df
Luca <luca.scodellaro@cern.ch>
2021-09-16 18:08:44 +0200

    submitSusySteps.sh improvements

--- NanoGardener/scripts/submitSusySteps.sh
+++ NanoGardener/scripts/submitSusySteps.sh
@@ -1,66 +1,179 @@
 #!/bin/bash
 
-prod=$1
-step=$2
+baseDirectory='/gpfs/projects/tier3data/LatinosSkims/RunII/Nano/'
 
-queue=cms_med
-if [ $step == 'sel' ]; then
-    queue=cms_main
-elif [ $step == 'data' ]; then
-    queue=cms_main
-fi
+prods=$1
+steps=$2
 
-sample=''
+isAllDone () {
 
-if [ $# -gt 2 ]; then
-    if [[ $3 == *' '* ]]; then
-        sample=$3
-    else
-        queue=cms_$3
-    fi 
-    if [ $# -gt 3 ]; then
-        sample=$4
+    datasetsToExclude=$sample
+ 
+    inputDirectory=$baseDirectory/$2/$3/
+    if [ -d "$inputDirectory" ]; then
+        inputFiles=$(ls $inputDirectory/*root | grep -c root)
+        if [ "$inputFiles" == "0" ] ; then
+            return 1
+        fi
+    else 
+        return 1
     fi
-fi
 
-naod='nAODv8'
+    if [[ $1 == 'hadd' || $1 == 'corr' ]] ; then
+        # This is treaky
+        if [[ "$datasetsToExclude" == *"-T "* ]] ; then
+            echo Will not proceed with submission for $2 step $4: option -T not supported by this script for the required step
+            return 2
+        fi
+        ls $inputDirectory | grep .root > filesToHadd_${2}_${3}.txt
+        dataset=""
+        noAvailableDatasets=1
+        while read line ; do
+            line="${line/nanoLatino_/}"
+            line=${line%__part*}
+            if [ "$line" != "$dataset" ] ; then
+                datasetFiles=$(ls $inputDirectory/nanoLatino_${line}__part*.root | grep -c root)
+                missingFiles=0
+                for i in $(seq $datasetFiles); do 
+                    if [ ! -f "$inputDirectory/nanoLatino_${line}__part$((i-1)).root" ]; then
+                        missingFiles=1
+                    fi
+                done
+                if  [ "$missingFiles" == "1" ] ; then
+                    if [ "$datasetsToExclude" != "" ] ; then
+                        datasetsToExclude=${datasetsToExclude},
+                    fi
+                    datasetsToExclude=$datasetsToExclude$line
+                else
+                    noAvailableDatasets=0
+                fi
+                dataset=$line
+            fi
+        done < filesToHadd_${2}_${3}.txt
+        if [[ "$datasetsToExclude" != "" &&  "$datasetsToExclude" != *"-E "* ]] ; then
+            datasetsToExclude="-b -E "$datasetsToExclude
+        fi
+        return $noAvailableDatasets
+    else
+        outputDirectory=$baseDirectory/$2/${3}__${4}/
+        if [ -d "$outputDirectory" ]; then
+            outputFiles=$(ls $outputDirectory/*root | grep -c root)
+            if [[ $inputFiles -eq $outputFiles ]] ; then
+                return 1
+            else
+                return 0
+            fi
+        else 
+            return 0
+        fi
+    fi 
 
-if [ $prod == '16HIPM' ]; then
-    year='16'
-    naod='nAODv9_HIPM'
-elif [ $prod == '16noHIPM' ]; then
-    year='16'
-    naod='nAODv9_noHIPM'
-else
-    year=$prod
-fi
+}
 
-if [ $step == 'data' ]; then
+submitJobs () {
 
-    ./mkPostProc.py -p Run20${year}_106X_${naod}_Full20${year}v8 -s DATASusy20${year}v8 -b -Q $queue $sample
+    if [[ $1 == 'lep' || $1 == 'sel' ]] ; then
+        ./mkPostProc.py -p $2 -s $3 -b -Q $queue $sample
+    else
+        isAllDone $step $2 $3 $4
+        allDone=$?
+        if [ "$allDone" == "0" ] ; then
+            ./mkPostProc.py -p $2 -i $3 -s $4 -b -Q $queue $datasetsToExclude
+        elif [ "$allDone" == "1" ] ; then
+            echo Nothing to submit for $2 step $4
+        fi
+    fi
 
-elif [ $step == 'sel' ]; then
+}
 
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -s MCSusy20${year}v8 -b -Q $queue $sample
+for prod in 16HIPM 16noHIPM 17 18 ; do
+    if [[ $prods == 'all' || $prods == *${prod}* ]]; then
+       
+        dataSteps=(lep hadd mt2)
+        mcSteps=(sel corr syst reco ctrl)
+        for step in ${dataSteps[@]} ${mcSteps[@]}; do
+            stepType=''
+            if echo ${dataSteps[@]} | grep -w -q $step ; then
+                stepType=data
+            elif echo ${mcSteps[@]} | grep -w -q $step ; then
+                stepType=mc
+            fi
+            if [[ $steps == 'all' || $steps == $stepType || $steps == $step ]] ; then
 
-elif [ $step == 'corr' ]; then
+                queue=cms_med
+                if [[ $step == 'lep' || $step == 'sel' ]]; then
+                    queue=cms_main
+                elif [[ $step == 'hadd' || $step == 'mt2' || $step == 'reco' || $step == 'ctrl' ]]; then
+                    queue=cms_high
+                fi
 
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8 -s MCSusyCorr20${year}v8 -b -Q $queue $sample
+                sample=''
+                if [ $# -gt 2 ]; then
+                    if [[ $3 == *' '* ]]; then
+                        sample=$3
+                    else
+                        queue=cms_$3
+                    fi 
+                    if [ $# -gt 3 ]; then
+                        sample=$4
+                    fi
+                fi
 
-elif [ $step == 'syst' ]; then
+                naod='nAODv8'
 
-    for syst in Nomin JESUp JESDo ; do
-        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8 -s MCSusy${syst}20${year}v8 -b -Q $queue $sample
-    done
+                if [ $prod == '16HIPM' ]; then
+                    year='16'
+                    naod='nAODv9_HIPM'
+                elif [ $prod == '16noHIPM' ]; then
+                    year='16'
+                    naod='nAODv9_noHIPM'
+                else
+                    year=$prod
+                fi
 
-elif [[ $step == 'reco' ]] || [[ $step == 'ctrl' ]] ; then
+                if [ $step == 'lep' ]; then
 
-    for met in Nomin Smear SMTUp SMTDo ; do
-        ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyNomin20${year}v8 -s susyMT2${step}$met -b -Q $queue $sample
-    done
+                    submitJobs $step Run20${year}_106X_${naod}_Full20${year}v8 DATASusy20${year}v8
 
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESUp20${year}v8 -s susyMT2${step}SJSUp -b -Q $queue $sample
-    ./mkPostProc.py -p Summer20UL${year}_106X_${naod}_Full20${year}v8 -i MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESDo20${year}v8 -s susyMT2${step}SJSDo -b -Q $queue $sample
+                elif [ $step == 'hadd' ]; then
 
-fi
+                    submitJobs $step Run20${year}_106X_${naod}_Full20${year}v8 DATASusy20${year}v8 hadd
+
+                elif [ $step == 'mt2' ]; then
+
+                    submitJobs $step Run20${year}_106X_${naod}_Full20${year}v8 DATASusy20${year}v8__hadd susyMT2recoNomin
+                    submitJobs $step Run20${year}_106X_${naod}_Full20${year}v8 DATASusy20${year}v8__hadd susyMT2ctrlNomin
+
+                elif [ $step == 'sel' ]; then
+
+                    submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8
+
+                elif [ $step == 'corr' ]; then
+
+                    submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8 MCSusyCorr20${year}v8
+
+                elif [ $step == 'syst' ]; then
+
+                    for syst in Nomin JESUp JESDo ; do 
+                        submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8__MCSusyCorr20${year}v8 MCSusy${syst}20${year}v8
+                    done
+
+                elif [[ $step == 'reco' ]] || [[ $step == 'ctrl' ]] ; then
+
+                    for met in Nomin Smear SMTUp SMTDo ; do
+                        submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyNomin20${year}v8 susyMT2${step}$met
+                    done
+
+                    submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESUp20${year}v8 susyMT2${step}SJSUp
+                    submitJobs $step Summer20UL${year}_106X_${naod}_Full20${year}v8 MCSusy20${year}v8__MCSusyCorr20${year}v8__MCSusyJESDo20${year}v8 susyMT2${step}SJSDo
+
+                fi
+
+
+            fi
+        done
+
+    fi
+done
+
 
