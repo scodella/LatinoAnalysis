82eadf1a895433835abc8468a4183301825716dc
scodella <luca.scodellaro@cern.ch>
2021-10-01 18:41:18 +0200

    fix UL sample name

--- NanoGardener/python/framework/samples/Run2016_UL2016_noHIPM_nAODv9.py
+++ NanoGardener/python/framework/samples/Run2016_UL2016_noHIPM_nAODv9.py
@@ -1,12 +1,12 @@
 Samples = {}
 
 Samples["DoubleEG_Run2016F_UL2016-v1"] 	   	       = {'nanoAOD' : '/DoubleEG/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-#Samples["DoubleEG_Run2016G_UL2016-v1"]                 = {'nanoAOD' : '/DoubleEG/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-Samples["DoubleEG_Run2016G_UL2016-v2"]                 = {'nanoAOD' : '/DoubleEG/Run2016G-UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD'}
+Samples["DoubleEG_Run2016G_UL2016-v1"]                 = {'nanoAOD' : '/DoubleEG/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 Samples["DoubleEG_Run2016H_UL2016-v1"]                 = {'nanoAOD' : '/DoubleEG/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 
 Samples["DoubleMuon_Run2016F_UL2016-v1"] 	       = {'nanoAOD' : '/DoubleMuon/Run2016F-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
-Samples["DoubleMuon_Run2016G_UL2016-v1"] 	       = {'nanoAOD' : '/DoubleMuon/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+#Samples["DoubleMuon_Run2016G_UL2016-v1"] 	       = {'nanoAOD' : '/DoubleMuon/Run2016G-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
+Samples["DoubleMuon_Run2016G_UL2016-v2"]              = {'nanoAOD' : '/DoubleMuon/Run2016G-UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD'}
 Samples["DoubleMuon_Run2016H_UL2016-v1"]               = {'nanoAOD' : '/DoubleMuon/Run2016H-UL2016_MiniAODv2_NanoAODv9-v1/NANOAOD'}
 
 Samples["MET_Run2016F_UL2016-v2"] 	    	       = {'nanoAOD' : '/MET/Run2016F-UL2016_MiniAODv2_NanoAODv9-v2/NANOAOD'}
