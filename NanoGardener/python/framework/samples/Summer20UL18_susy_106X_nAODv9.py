702af0ed8678cd8a89e17d3bb58455dc623f8abc
scodella <luca.scodellaro@cern.ch>
2021-11-26 09:40:31 +0100

    more ULs

--- NanoGardener/python/framework/samples/Summer20UL18_susy_106X_nAODv9.py
+++ NanoGardener/python/framework/samples/Summer20UL18_susy_106X_nAODv9.py
@@ -59,7 +59,7 @@ Samples['VBFHToTauTau_M125'] 	 = {'nanoAOD': '/VBFHToTauTau_M125_TuneCP5_13TeV-p
 
 #Samples['WJetsToLNu-LO'] 	 = {'nanoAOD': '/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM'}
 ##Samples['WJetsToLNu_HT100_200'] 	 = {'nanoAOD': ''}
-##Samples['WJetsToLNu_HT1200_2500'] 	 = {'nanoAOD': ''}
+#Samples['WJetsToLNu_HT1200_2500'] 	 = {'nanoAOD': '/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM'}
 #Samples['WJetsToLNu_HT200_400'] 	 = {'nanoAOD': '/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM'}
 ##Samples['WJetsToLNu_HT2500_inf'] 	 = {'nanoAOD': ''}
 #Samples['WJetsToLNu_HT400_600'] 	 = {'nanoAOD': '/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM'}
