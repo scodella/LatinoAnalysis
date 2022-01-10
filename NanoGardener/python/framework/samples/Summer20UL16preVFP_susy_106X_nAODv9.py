9a539c922177191757d50f470029378efa8f3e13
Luca <luca.scodellaro@cern.ch>
2022-01-10 10:05:22 +0100

    update ULs

--- NanoGardener/python/framework/samples/Summer20UL16preVFP_susy_106X_nAODv9.py
+++ NanoGardener/python/framework/samples/Summer20UL16preVFP_susy_106X_nAODv9.py
@@ -18,7 +18,7 @@ Samples['DYJetsToLL_M-50_HT-600to800'] 	 = {'nanoAOD': '/DYJetsToLL_M-50_HT-600t
 Samples['DYJetsToLL_M-50_HT-70to100'] 	 = {'nanoAOD': '/DYJetsToLL_M-50_HT-70to100_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM'}
 Samples['DYJetsToLL_M-50_HT-800to1200'] 	 = {'nanoAOD': '/DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM'}
 
-#Samples['GluGluHToTauTau_M125'] 	 = {'nanoAOD': ''}
+Samples['GluGluHToTauTau_M125'] 	 = {'nanoAOD': '/GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM'}
 Samples['GluGluHToWWTo2L2Nu_M125'] 	 = {'nanoAOD': '/GluGluHToWWTo2L2Nu_M125_TuneCP5_13TeV_powheg2_JHUGenV714_pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM'}
 #Samples['GluGluHToZZTo4L_M125'] 	 = {'nanoAOD': ''}
 Samples['GluGluToWWToENEN'] 	 = {'nanoAOD': '/GluGluToWWToENEN_TuneCP5_13TeV_MCFM701_pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM'}
