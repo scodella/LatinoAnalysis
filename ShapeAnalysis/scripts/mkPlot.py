c37db32914466ce8bceeb153f6139ac0cbfc77e1
Luca <luca.scodellaro@cern.ch>
2022-02-01 19:10:22 +0100

    adjust mkPlot for ptmiss-dependent mt2ll binning

--- ShapeAnalysis/scripts/mkPlot.py
+++ ShapeAnalysis/scripts/mkPlot.py
@@ -201,15 +201,15 @@ if __name__ == '__main__':
       exec(handle)
       handle.close()
 
-    variables = {}
-    if os.path.exists(opt.variablesFile) :
-      handle = open(opt.variablesFile,'r')
-      exec(handle)
-      handle.close()
-   
     cuts = {}
     if os.path.exists(opt.cutsFile) :
       handle = open(opt.cutsFile,'r')
+      exec(handle)
+      handle.close()
+
+    variables = {}
+    if os.path.exists(opt.variablesFile) :
+      handle = open(opt.variablesFile,'r')
       exec(handle)
       handle.close()
 
