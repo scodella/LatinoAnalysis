0eb0c2c3b982a0d4d2dc40eabd875ebe24c8ddde
scodella <luca.scodellaro@cern.ch>
2022-10-04 19:04:42 +0200

    FS UL

---
+++ NanoProducer/python/samples/Spring21UL16FS_106X_mAODv2.py
@@ -0,0 +1,6 @@
+Samples = {}
+
+# T2tt
+Samples['T2tt_mStop-400to825'] = {'miniAOD': '/SMS-T2tt_mStop-400to825_dM-87to175_2Lfilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16MiniAODv2-FSUL16_106X_mcRun2_asymptotic_v17-v1/MINIAODSIM', 'customise' : [] }
+
+
