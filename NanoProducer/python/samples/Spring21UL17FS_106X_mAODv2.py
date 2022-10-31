3bfa1390d9b14cc289b94dc08138d9f0191cddef
Luca <luca.scodellaro@cern.ch>
2022-10-31 17:12:58 +0100

    UL FS mini

---
+++ NanoProducer/python/samples/Spring21UL17FS_106X_mAODv2.py
@@ -0,0 +1,6 @@
+Samples = {}
+
+# T2tt
+Samples['T2tt_mStop-400to825'] = {'miniAOD': '/SMS-T2tt_mStop-400to825_dM-87to175_2Lfilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL17MiniAODv2-FSUL17_106X_mc2017_realistic_v9-v2/MINIAODSIM', 'customise' : [] }
+
+
---
+++ NanoProducer/python/samples/Spring21UL18FS_106X_mAODv2.py
@@ -0,0 +1,6 @@
+Samples = {}
+
+# T2tt
+Samples['T2tt_mStop-400to825'] = {'miniAOD': '/SMS-T2tt_mStop-400to825_dM-87to175_2Lfilter_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAODv2-FSUL18_106X_upgrade2018_realistic_v16_L1v1-v3/MINIAODSIM', 'customise' : [] }
+
+
