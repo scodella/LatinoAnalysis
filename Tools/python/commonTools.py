61881947d8b02e3eac8b77954665adc71ad9f9eb
scodella <luca.scodellaro@cern.ch>
2023-11-30 14:43:57 +0100

    new BTA name format

--- Tools/python/commonTools.py
+++ Tools/python/commonTools.py
@@ -262,6 +262,13 @@ def getSampleFiles(inputDir,Sample,absPath=False,rooFilePrefix='latino_',FromPos
       proc    = subprocess.Popen(fileCmd, stderr = subprocess.PIPE,stdout = subprocess.PIPE, shell = True)
       out,err = proc.communicate()
       Files   = string.split(out)
+    if len(Files) == 0 : # BTagPerf
+      fileCmd = lsCmd+Dir+'/'+rooFilePrefix+Sample+'MC_*_*_*.root'
+      if 'root://' in inputDir:
+        fileCmd = lsCmd+Dir+'/ | grep '+rooFilePrefix+Sample+'MC_ | grep root'
+      proc    = subprocess.Popen(fileCmd, stderr = subprocess.PIPE,stdout = subprocess.PIPE, shell = True)
+      out,err = proc.communicate()
+      Files   = string.split(out)
     if len(Files) == 0 and not FromPostProc :
       print 'ERROR: No files found for sample ',Sample,' in directory ',Dir
       exit() 
